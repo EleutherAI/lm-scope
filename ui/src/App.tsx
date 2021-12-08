214d51af5aad6a40bbc63419e3ceb8d18711515b
Jacob Merizian <jmerizia@vt.edu>
2021-12-08 09:06:03 -0500

    nav

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+  } from "react-router-dom";
+import Home from './Home';
+  import Layout from "./Layout";
+
 
 
 function App() {
     return (
-        <div>hi</div>
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path={`${process.env.PUBLIC_URL}/viewer`}
+                    element={<Layout>
+                        <div>viewer</div>
+                    </Layout>}
+                />
+                <Route
+                    path={`${process.env.PUBLIC_URL}/`}
+                    element={<Layout>
+                        <Home />
+                    </Layout>}
+                />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
---
+++ ui/src/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Home() {
+    return <div className='main-page'>
+        <br />
+        <h1>
+            LM-Scope
+        </h1>
+        <p>
+            todo
+        </p>
+    </div>;
+}
+
+export default Home;
